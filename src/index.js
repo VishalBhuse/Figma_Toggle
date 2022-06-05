b88c7a8cf875c04e7e4346e773ca411abd9fbf6e
VishalBhuse <vbhuse1997@gmail.com>
2022-06-05 14:40:00 -0700

    first sample

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
+import { FigmaProvider } from './context/Figmacontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ChakraProvider>
+      <FigmaProvider>
+
     <App />
+      </FigmaProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
